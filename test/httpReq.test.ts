94c160df79020c060cc5c5f43ccc3dc4ec0aeb4b
hmdnu <daniuyan71@gmail.com>
2025-02-01 10:10:49 +0700

    feat: request wrapped promise

--- test/httpReq.test.ts
+++ test/httpReq.test.ts
@@ -1,7 +1,6 @@
-import { describe, it, expect, vi, expectTypeOf, beforeEach } from "vitest";
+import { describe, it, expect, vi, beforeEach } from "vitest";
 import { HttpRequest } from "@/cores/HttpRequest";
 import { wrapPromise } from "@/utils/promise";
-import { ofetch } from "node_modules/ofetch/dist/node.cjs";
 
 describe("HttpRequest_html", () => {
   let httpRequest: HttpRequest;
@@ -21,6 +20,35 @@ describe("HttpRequest_html", () => {
     const html = await httpRequest.html("https://mockurl.com");
     expect(html).toSatisfy(
       (res) => typeof res === "string" || typeof res === null,
+    );
+  });
+
+  it("should return html string in wrapped promise", async () => {
+    const mockHtml = { res: "<html>", err: null };
+
+    global.fetch = vi.fn(() =>
+      Promise.resolve(new Response(JSON.stringify(mockHtml), { status: 200 })),
+    );
+    const { res, err } = await wrapPromise(
+      httpRequest.html("https://mockurl.com"),
+    );
+    expect(err).toBeNull();
+    expect(res).toBeDefined();
+    expect(res).toEqual(mockHtml);
+  });
+
+  it("should return valid error type when fetching html in wrapped promise", async () => {
+    global.fetch = vi.fn(() => Promise.reject(new Error()));
+
+    const { res, err } = await wrapPromise(
+      httpRequest.html("https://mockurl.com"),
+    );
+
+    expect(res).toBeNull();
+    expect(err).toBeInstanceOf(Error);
+    expect(err).toBeDefined();
+    expect((err as Error)?.message).toSatisfy(
+      (err) => typeof err === "string" && err === "<unknown error>",
     );
   });
 });
